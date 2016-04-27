60a67441694febe8d8ace34822140f0d5f027a09
Juan Domenech Fernandez <juan.domenech@gmail.com>
2016-04-27 19:09:12 +0100

    New app Accounts

---
+++ accounts/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from django.contrib import messages, auth
+from django.template.context_processors import csrf
+from accounts.forms import UserRegistrationForm, UserLoginForm
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(email=request.POST.get('email'),
+                                     password=request.POST.get('password'))
+
+            if user is not None:
+                auth.login(request, user)
+                messages.error(request, "You have successfully logged in")
+                return redirect(reverse('profile'))
+            else:
+                form.add_error(None, "Your email or password was not recognised")
+
+    else:
+        form = UserLoginForm()
+
+    args = {'form': form}
+    args.update(csrf(request))
+    return render(request, 'login.html', args)
